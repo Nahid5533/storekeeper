64e2be5cdf702d2f671b9959919495880017e743
MD Nahid <144885398+Nahid5533@users.noreply.github.com>
2023-12-17 23:59:18 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Routes for adding products
+
 Route::get('/products/create', [ProductController::class, 'showCreateForm']);
 Route::post('/products', [ProductController::class, 'addProduct'])->name('products.store');
 
-// Routes for changing product prices
+
 Route::get('/products/{product}/edit-price', [PriceController::class, 'showPriceForm']);
 Route::put('/products/{product}/update-price', [PriceController::class, 'updatePrice'])->name('products.updatePrice');
 
-// Dashboard route
+
 Route::get('/dashboard', [DashboardController::class, 'showDashboard']);
 
-// Transaction history route
+
 Route::get('/transactions', [TransactionController::class, 'showTransactionHistory']);
